d553ea28fea8595d3a2013a0c6dfb7ed2692a434
Simon Kronstein <skronstein7@gmail.com>
2020-04-04 15:05:41 -0700

    Python - add comment explaining that the save_to_usb example will save a region 500 frames long, starting at frame 300.

--- python3/save_to_usb.py
+++ python3/save_to_usb.py
@@ -2,6 +2,7 @@
 
 import requests
 
+# Save a region 500 frames long, starting at frame 300.
 post = requests.post('http://192.168.12.1/control/startFilesave', json = {'format': 'h264', 'device': 'sda1', 'start': 300, 'length': 500})
 if post.reason != "OK" :
 	print("Could not save recording. Is a storage device inserted?")
