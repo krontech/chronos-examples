a237ddc48acc052838c261a3f8185a31fc1e1fbd
Simon Kronstein <skronstein7@gmail.com>
2020-04-21 20:50:48 -0700

    There was a change made to the API where the startRecording method now requires an input parameter set. This commit adds the newly required parameter set (which is empty) to the POST request for startRecording.

--- python3/start_stop_recording.py
+++ python3/start_stop_recording.py
@@ -3,7 +3,7 @@
 import time
 import requests
 
-post = requests.post('http://192.168.12.1/control/startRecording')
+post = requests.post('http://192.168.12.1/control/startRecording', json = {})
 time.sleep(2)
 post = requests.post('http://192.168.12.1/control/stopRecording')
 
