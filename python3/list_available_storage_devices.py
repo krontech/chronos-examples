4eb6622307d48b1c5185d2df9c9fe9a81a8ae129
Simon Kronstein <skronstein7@gmail.com>
2020-05-26 03:53:20 -0700

    fix list_available_storage_devices.py
    API was updated. The old call to /control/p/externalStorage had to be removed. This commit updates the python script to check available storage through /cgi-bin instead.

--- python3/list_available_storage_devices.py
+++ python3/list_available_storage_devices.py
@@ -2,6 +2,6 @@
 
 import requests
 
-post = requests.get('http://192.168.12.1/control/p/externalStorage')
+post = requests.get('http://192.168.12.1/cgi-bin/storageInfo?mmcblk1p1')
 print(post.json())
 
