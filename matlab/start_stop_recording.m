a8aeb4b119dd5f06aae3effc38282f76e8dd023e
Eugene <97128575+EugeneSolomko@users.noreply.github.com>
2023-06-20 10:36:01 -0700

    Update start_stop_recording.m
    changed urlread to webwrite

--- matlab/start_stop_recording.m
+++ matlab/start_stop_recording.m
@@ -1,3 +1,20 @@
-status = urlread('http://192.168.12.1/control/startRecording','post','{}');
+% 192.168.12.1 is the IP address of a camera when connected via USB.
+cameraStartRecording = 'http://192.168.12.1/control/startRecording';
+cameraStopRecording = 'http://192.168.12.1/control/stopRecording';
+
+% Set Content-Type using HeaderFields instead of MediaType
+% to avoid having a semicolon after json in CharacterEncoding,
+% which would cause Error 400 Bad Request.
+options = weboptions('HeaderFields',{'Content-Type' 'application/json'});
+
+% EMPTY DATA STRUCTURE 
+data = struct();
+
+% Start recording
+responseStart = webwrite(cameraStartRecording,data,options);
+
+% PAUSE
 pause(2);
-status = urlread('http://192.168.12.1/control/stopRecording','post','');
+
+% Stop recording
+responseStop = webwrite(cameraStopRecording,data,options);
